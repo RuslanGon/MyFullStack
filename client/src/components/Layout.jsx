96c1abf9b3ceb3c926ba4ed58cda01d3bb842d60
RuslanGon <ruslan441006@gmail.com>
2025-12-27 16:29:06 +0200

    wer

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import css from './Layout.module.css';
 import { useSelector } from 'react-redux';
-import { selectAuthIsSignedIn } from '../redux/auth/selectors.js';
+import { selectAuthIsSignedIn, selectAuthUserData } from '../redux/auth/selectors.js';
 
 const navLinkClass = ({ isActive }) => 
   isActive ? `${css['nav-link']} ${css.active}` : css['nav-link'];
@@ -10,29 +10,33 @@ const navLinkClass = ({ isActive }) =>
 const Layout = ({ children }) => {
 
 const isSignedIn = useSelector(selectAuthIsSignedIn)  
-
+const userData = useSelector(selectAuthUserData)
 
   return (
     <div className={css.layout}>
-      <header className={css.header}>
-        <nav className={css.nav}>
-          {isSignedIn ? (
-            <>
-               <NavLink to="/" className={navLinkClass}>Home Page</NavLink>
+    <header className={css.header}>
+      <nav className={css.nav}>
+        {isSignedIn ? (
+          <>
+            <div className={css.navLinks}>
+              <NavLink to="/" className={navLinkClass}>Home Page</NavLink>
               <NavLink to="/cars" className={navLinkClass}>Cars</NavLink>
-              <NavLink to="/contacts" className={navLinkClass}>Contacts </NavLink>
-            </>
-          ) : (
-            <>
+              <NavLink to="/contacts" className={navLinkClass}>Contacts</NavLink>
+            </div>
+            <div className={css.userGreeting}>
+              <p>Hello, {userData.name}</p>
+            </div>
+          </>
+        ) : (
+          <div className={css.navLinks}>
             <NavLink to="/login" className={navLinkClass}>Login</NavLink>
-            <NavLink to="/register" className={navLinkClass}> Register</NavLink>
-             
-            </>
-          )}
-        </nav>
-      </header>
-      <main className={css.main}>{children}</main>
-    </div>
+            <NavLink to="/register" className={navLinkClass}>Register</NavLink>
+          </div>
+        )}
+      </nav>
+    </header>
+    <main className={css.main}>{children}</main>
+  </div>
   );
 }
 
