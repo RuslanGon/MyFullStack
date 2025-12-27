2ecde9ed78673292b193fff44118787df6320934
RuslanGon <ruslan441006@gmail.com>
2025-12-27 18:43:37 +0200

    wer

--- client/src/pages/ContactsPage.jsx
+++ client/src/pages/ContactsPage.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { selectContactsCars, selectContactsIsError, selectContactsIsLoading } from '../redux/contacts/selectors.js'
+import { apiGetContacts } from '../redux/contacts/operations.js'
 
 const ContactsPage = () => {
+  const dispatch = useDispatch()
+  const contacts = useSelector(selectContactsCars)
+  const loading = useSelector(selectContactsIsLoading)
+  const error = useSelector(selectContactsIsError)
+
+  useEffect(() => {
+    console.log("Dispatch contacts...");
+    dispatch(apiGetContacts());
+  }, [dispatch]);  
+
   return (
-    <div>ContactsPage</div>
+
+    <div>
+       {loading && <div>...Loading</div>}
+        {error && <div>...error</div>}
+    </div>
   )
 }
 
--- client/src/redux/contacts/operations.js
+++ client/src/redux/contacts/operations.js
@@ -2,15 +2,25 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'https://connections-api.goit.global'
-})
+  baseURL: 'https://connections-api.goit.global'
+});
 
-export const apiGetContacts = createAsyncThunk('getAll/contacts', async (_, thunApi) => {
+export const apiGetContacts = createAsyncThunk(
+  'contacts/getAll',
+  async (_, thunkApi) => {
     try {
-        const {data} = await instance.get('/contacts')
-        console.log(data);
-        return data
+      const state = thunkApi.getState();
+      const token = state.auth.token;  
+
+      if (!token) {
+        return thunkApi.rejectWithValue("No token");
+      }
+      instance.defaults.headers.common.Authorization = `Bearer ${token}`;
+      const { data } = await instance.get('/contacts');
+      return data;
+
     } catch (error) {
-        return thunApi.rejectWithValue(error.message)
+      return thunkApi.rejectWithValue(error.response?.data || error.message);
     }
-})
+  }
+);
