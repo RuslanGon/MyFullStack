d8b05bf0cbc2008d725382eed487b0b2d3bd217e
RuslanGon <ruslan441006@gmail.com>
2026-01-04 18:33:43 +0200

    were

--- server/routes/students.js
+++ server/routes/students.js
@@ -2,6 +2,8 @@ import { Router } from "express";
 import { createStudentController, deleteStudentByIdController, getAllStudentsController, getStudentByIdController, patchStudentController, putStudentController,  } from "../controllers/students.js";
 import { ctrllWrapper } from "../src/utils/ctrlWrapper.js";
 import { validateMongoId } from "../src/middlewars/validateMongoId.js";
+import { validateBody } from "../src/middlewars/validateBody.js";
+import { createStudentSchema } from "../validationSchema/createStudentSchema.js";
 
 
 const studentsRouter = Router();
@@ -15,6 +17,7 @@ studentsRouter.get(
 );
 
 studentsRouter.post('/students',
+validateBody(createStudentSchema),
 ctrllWrapper(createStudentController));
 
 studentsRouter.delete(
