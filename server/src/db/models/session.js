6e8a6bcb6bad935522461794d03bab1c8c8db943
RuslanGon <ruslan441006@gmail.com>
2026-01-07 16:22:39 +0200

    were

---
+++ server/src/db/models/session.js
@@ -0,0 +1,17 @@
+import { Schema, model } from 'mongoose';
+
+const sessionSchema = new Schema({
+    refreshToken: { type: String, required: true },
+    accessToken: { type: String, required: true },
+    refreshTokenValidUntil: { type: String, required: true },
+    accessTokenValidUntil: { type: String, required: true },
+    userId: { type: Schema.ObjectId, required: true, unique: true },
+
+  },
+  {
+    timestamps: true,
+  },
+);
+
+
+export const Session = model('sessions', sessionSchema);
--- server/src/services/auth.js
+++ server/src/services/auth.js
@@ -2,6 +2,7 @@ import createHttpError from "http-errors";
 import { User } from "../db/models/user.js";
 import bcrypt from "bcrypt";
 import crypto from 'crypto';
+import { Session } from "../db/models/session.js";
 
 export const createUser = async (payload) => {
   const hashedPassword = await bcrypt.hash(payload.password, 10);
@@ -13,22 +14,31 @@ export const createUser = async (payload) => {
 };
 
 export const loginUser = async ({ email, password }) => {
-    const user = await User.findOne({ email });
-    if (!user) {
-      throw createHttpError(404, 'User not found');
-    }
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw createHttpError(404, 'User not found');
+  }
 
-    const areEqual = await bcrypt.compare(password, user.password);
-    if (!areEqual) {
-      throw createHttpError(401, 'Email or password is wrong');
-    }
+  const areEqual = await bcrypt.compare(password, user.password);
+  if (!areEqual) {
+    throw createHttpError(401, 'Email or password is wrong');
+  }
 
-    const accessToken = crypto.randomBytes(30).toString('hex');
-    const refreshToken = crypto.randomBytes(40).toString('hex');
+  const accessToken = crypto.randomBytes(30).toString('hex');
+  const refreshToken = crypto.randomBytes(40).toString('hex');
 
-    return {
-      user,
-      accessToken,
-      refreshToken,
-    };
+  const session = await Session.create({
+    userId: user._id,
+    accessToken,
+    refreshToken,
+    accessTokenValidUntil: new Date(Date.now() + 15 * 60 * 1000),
+    refreshTokenValidUntil: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
+  });
+
+  return {
+    user,
+    accessToken: session.accessToken,
+    refreshToken: session.refreshToken,
   };
+};
+
