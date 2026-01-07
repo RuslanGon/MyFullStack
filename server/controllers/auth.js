288a1e1b523d1aeec5875707cccf5aa1913d3ae4
RuslanGon <ruslan441006@gmail.com>
2026-01-07 09:55:09 +0200

    wre

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -2,6 +2,7 @@ import { createUser } from "../src/services/auth.js";
 
 export const registerController = async (req,res, next) => {
 const user = await createUser(req.body);
+
 res.json({
     status: 200,
     message: 'Register new user',
--- server/src/services/auth.js
+++ server/src/services/auth.js
@@ -1,5 +1,11 @@
 import { User } from "../db/models/user.js";
+import bcrypt from "bcrypt";
 
 export const createUser = async (payload) => {
-return await User.create(payload);
+  const hashedPassword = await bcrypt.hash(payload.password, 10);
+
+  return await User.create({
+    ...payload,
+    password: hashedPassword,
+  });
 };
