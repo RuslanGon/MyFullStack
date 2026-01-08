0087381962597701a1ef0941617889f9aa9925ff
RuslanGon <ruslan441006@gmail.com>
2026-01-08 17:50:20 +0200

    wrr

---
+++ server/src/middlewars/authenticate.js
@@ -0,0 +1,50 @@
+import { Session } from '../db/models/session.js';
+
+export const authenticate = async (req, res, next) => {
+  try {
+    const header = req.get('Authorization');
+
+    if (!header) {
+      return res.status(401).json({
+        status: 401,
+        message: 'Authorization header is not provided',
+      });
+    }
+
+    const [bearer, token] = header.split(' ');
+
+    if (bearer !== 'Bearer' || !token) {
+      return res.status(401).json({
+        status: 401,
+        message: 'Authorization header must be of Bearer type',
+      });
+    }
+
+    const session = await Session.findOne({ accessToken: token });
+
+    if (!session) {
+      return res.status(401).json({
+        status: 401,
+        message: 'Session not found',
+      });
+    }
+
+    if (new Date() > session.accessTokenValidUntil) {
+      return res.status(401).json({
+        status: 401,
+        message: 'Access token expired',
+      });
+    }
+
+    req.userId = session.userId;
+    req.session = session;
+
+    next();
+  } catch (err) {
+    console.error('Authenticate error:', err);
+    return res.status(500).json({
+      status: 500,
+      message: 'Internal server error',
+    });
+  }
+};
