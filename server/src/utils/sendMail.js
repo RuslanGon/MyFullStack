0c2157db36ab2a29cb0b1c080f4f54a78bbe8f56
RuslanGon <ruslan441006@gmail.com>
2026-01-12 14:01:11 +0200

    wer

--- server/src/utils/sendMail.js
+++ server/src/utils/sendMail.js
@@ -1,11 +1,10 @@
 import nodemailer from 'nodemailer';
 
 export const transporter = nodemailer.createTransport({
-    host: "smtp.ethereal.email",
-    port: 587,
-    secure: false,
-    auth: {
-      user: "maddison53@ethereal.email",
-      pass: "jn7jnAPss4f63QBp6D",
-    },
-  });
+  host: 'smtp-relay.brevo.com',
+  port: 587,
+  auth: {
+    user: 'user',
+    pass: 'pass',
+  },
+});
