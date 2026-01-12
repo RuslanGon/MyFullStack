46a7a12061334f639729aa65cf08c6a5cd121abf
RuslanGon <ruslan441006@gmail.com>
2026-01-12 14:24:44 +0200

    wer

--- server/src/constants/index.js
+++ server/src/constants/index.js
@@ -4,9 +4,8 @@ DB_USER: 'practica',
 DB_PASSWORD: 'practica',
 DB_NAME: 'practica',
 
-
 SMTP_HOST: 'smtp-relay.brevo.com',
 SMTP_PORT: 587,
 SMTP_USER: '77ea97001@smtp-brevo.com',
-SMTP_PASSWORD: 'xsmtpsib-65d9b97b4cf6ed263b265976af0f559b69b7fc1bbd6b8500ae0721faaf58551a-2a3N6TDA1GqcYvLz',
+SMTP_PASSWORD: 'xsmtpsib-65d9b97b4cf6ed263b265976af0f559b69b7fc1bbd6b8500ae0721faaf58551a-VDAGEa5iC5a8TMSp',
 };
