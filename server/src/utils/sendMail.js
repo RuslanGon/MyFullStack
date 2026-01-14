ba5fede5c11d779a7ffeae9eaf6ce5f934ed5b88
RuslanGon <ruslan441006@gmail.com>
2026-01-14 15:05:27 +0200

    wee

--- server/src/utils/sendMail.js
+++ server/src/utils/sendMail.js
@@ -1,50 +1,50 @@
+// import nodemailer from 'nodemailer';
+// import { ENV_VARS } from '../constants/index.js';
+
+// const transporter = nodemailer.createTransport({
+//   host: ENV_VARS.SMTP_HOST,
+//   port: Number(ENV_VARS.SMTP_PORT),
+//   secure: false,      // false для STARTTLS
+//   requireTLS: true,   // форсировать TLS
+//   auth: {
+//     user: ENV_VARS.SMTP_USER,
+//     pass: ENV_VARS.SMTP_PASSWORD,
+//   },
+//   logger: true,
+//   debug: true
+// });
+
+// export const sendMail = async (options) => {
+//   return await transporter.sendMail({
+//     ...options,
+//     from: ENV_VARS.SMTP_USER, // от кого письмо
+//   });
+// };
+
 import nodemailer from 'nodemailer';
-import { ENV_VARS } from '../constants/index.js';
 
-const transporter = nodemailer.createTransport({
-  host: ENV_VARS.SMTP_HOST,
-  port: Number(ENV_VARS.SMTP_PORT),
-  secure: false,      // false для STARTTLS
-  requireTLS: true,   // форсировать TLS
-  auth: {
-    user: ENV_VARS.SMTP_USER,
-    pass: ENV_VARS.SMTP_PASSWORD,
-  },
-  logger: true,
-  debug: true
-});
+export const createTestTransporter = async () => {
+  const testAccount = await nodemailer.createTestAccount();
 
-export const sendMail = async (options) => {
-  return await transporter.sendMail({
-    ...options,
-    from: ENV_VARS.SMTP_USER, // от кого письмо
+  return nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    secure: false,
+    auth: {
+      user: testAccount.user,
+      pass: testAccount.pass,
+    },
   });
 };
 
-// import nodemailer from 'nodemailer';
+export const sendMail = async (options) => {
+  const transporter = await createTestTransporter();
 
-// export const createTestTransporter = async () => {
-//   const testAccount = await nodemailer.createTestAccount();
+  const info = await transporter.sendMail({
+    ...options,
+    from: `"Test" <${options.to}>`,
+  });
 
-//   return nodemailer.createTransport({
-//     host: 'smtp.ethereal.email',
-//     port: 587,
-//     secure: false,
-//     auth: {
-//       user: testAccount.user,
-//       pass: testAccount.pass,
-//     },
-//   });
-// };
-
-// export const sendMail = async (options) => {
-//   const transporter = await createTestTransporter();
-
-//   const info = await transporter.sendMail({
-//     ...options,
-//     from: `"Test" <${options.to}>`,
-//   });
-
-//   console.log('Preview URL:', nodemailer.getTestMessageUrl(info));
-//   return info;
-// };
+  console.log('Preview URL:', nodemailer.getTestMessageUrl(info));
+  return info;
+};
