65952072d4a76fe01e728ecbe3c04b010dca2e73
RuslanGon <ruslan441006@gmail.com>
2026-01-15 15:27:47 +0200

    wer

--- server/src/utils/createFolderTemp.js
+++ server/src/utils/createFolderTemp.js
@@ -3,13 +3,13 @@ import fs from 'node:fs/promises';
 export const createFolderTemp = async (dirPath) => {
   try {
     await fs.access(dirPath);
-    console.log(`Folder exists: ${dirPath}`);
+
   } catch (error) {
     if (error.code === 'ENOENT') {
       await fs.mkdir(dirPath, { recursive: true });
-      console.log(`Folder created: ${dirPath}`);
+
     } else {
-      console.error('Error checking folder:', error);
+
       throw error;
     }
   }
