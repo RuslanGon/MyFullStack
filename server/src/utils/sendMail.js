df273036fd3575f3f452d4768be174081f4548cf
RuslanGon <ruslan441006@gmail.com>
2026-01-15 09:45:10 +0200

    weer

--- server/src/utils/sendMail.js
+++ server/src/utils/sendMail.js
@@ -1,26 +1,3 @@
-// import nodemailer from 'nodemailer';
-// import { ENV_VARS } from '../constants/index.js';
-
-// const transporter = nodemailer.createTransport({
-//   host: ENV_VARS.SMTP_HOST,
-//   port: Number(ENV_VARS.SMTP_PORT),
-//   secure: false,      // false для STARTTLS
-//   requireTLS: true,   // форсировать TLS
-//   auth: {
-//     user: ENV_VARS.SMTP_USER,
-//     pass: ENV_VARS.SMTP_PASSWORD,
-//   },
-//   logger: true,
-//   debug: true
-// });
-
-// export const sendMail = async (options) => {
-//   return await transporter.sendMail({
-//     ...options,
-//     from: ENV_VARS.SMTP_USER, // от кого письмо
-//   });
-// };
-
 import nodemailer from 'nodemailer';
 
 export const createTestTransporter = async () => {
@@ -48,3 +25,26 @@ export const sendMail = async (options) => {
   console.log('Preview URL:', nodemailer.getTestMessageUrl(info));
   return info;
 };
+
+// import nodemailer from 'nodemailer';
+// import { ENV_VARS } from '../constants/index.js';
+
+// const transporter = nodemailer.createTransport({
+//   host: ENV_VARS.SMTP_HOST,
+//   port: Number(ENV_VARS.SMTP_PORT),
+//   secure: false,      // false для STARTTLS
+//   requireTLS: true,   // форсировать TLS
+//   auth: {
+//     user: ENV_VARS.SMTP_USER,
+//     pass: ENV_VARS.SMTP_PASSWORD,
+//   },
+//   logger: true,
+//   debug: true
+// });
+
+// export const sendMail = async (options) => {
+//   return await transporter.sendMail({
+//     ...options,
+//     from: ENV_VARS.SMTP_USER, // от кого письмо
+//   });
+// };
